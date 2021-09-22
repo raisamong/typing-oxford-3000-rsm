4ca8c496b81bdaf324ed1231584c36a56ca308fc
Thanawat Thangpiboonvech <thanawat.t@dv.co.th>
2021-09-22 21:15:29 +0700

    add mode state

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TypingInline from 'src/components/TypingInline'
+import TypingParagraph from 'src/components/TypingParagraph'
 import './App.css'
 
+const inline = 'inline'
+const paragraph = 'paragraph'
+
+type TypingMode = 'inline' | 'paragraph'
+
 function App() {
+  const [mode, setMode] = useState<TypingMode>('inline')
+
   return (
     <div className="app-container">
-      <button>Inline</button>
-      <button>Paragraph</button>
-      <TypingInline />
+      <button
+        onClick={() => {
+          setMode(inline)
+        }}
+      >
+        Inline
+      </button>
+      <button
+        onClick={() => {
+          setMode(paragraph)
+        }}
+      >
+        Paragraph
+      </button>
+
+      {mode === inline && <TypingInline />}
+      {mode === paragraph && <TypingParagraph />}
     </div>
   )
 }
