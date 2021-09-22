561fb1ae5cb29dfe4c0082de632d26496ed00038
Thanawat Thangpiboonvech <thanawat.t@dv.co.th>
2021-09-22 13:57:08 +0700

    add some styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
 function App() {
+  const textToType = 'All Hail Lelouch'
+  const [textTyping, setTextTyping] = useState<string>('')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="typing-container">
+        <div className="typed-text">
+          <p>{textToType.substring(0, textTyping.length)}</p>
+        </div>
+        <div></div>
+        <div className="incoming-text">
+          <p>{textToType.substring(textTyping.length, textToType.length)}</p>
+        </div>
+        <div></div>
+      </div>
+      <div className="typing-container">
+        <div className="typed-text">
+          <p>{textTyping.substring(textTyping.length - 30)}</p>
+        </div>
+        <div className="typewriter"></div>
+        <div className="incoming-text">
+          <p>{textToType.substring(textTyping.length, textToType.length)}</p>
+        </div>
+        <div></div>
+      </div>
+
+      <input
+        onChange={(e) => {
+          console.log(e.target.value)
+          setTextTyping(e.target.value)
+        }}
+      ></input>
+    </>
+  )
 }
 
-export default App;
+export default App
