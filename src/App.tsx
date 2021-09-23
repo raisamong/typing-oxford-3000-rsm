c65bac00cd4e29a652b009dc6b25685a2f769a02
Thanawat Thangpiboonvech <thanawat.t@dv.co.th>
2021-09-23 11:37:28 +0700

    text speech

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import TypingInline from 'src/components/TypingInline'
 import TypingParagraph from 'src/components/TypingParagraph'
+import { vocab } from 'src/vocab'
 import './App.css'
 
 const inline = 'inline'
@@ -10,6 +11,19 @@ type TypingMode = 'inline' | 'paragraph'
 
 function App() {
   const [mode, setMode] = useState<TypingMode>('inline')
+  const [speaker] = useState<SpeechSynthesisUtterance>(new SpeechSynthesisUtterance('test'))
+
+  // useEffect(() => {
+  //   setSpeaker()
+  // }, [])
+
+  const onClick = () => {
+    window.speechSynthesis.speak(speaker)
+  }
+  const onChange = () => {
+    speaker.text = vocab[Math.floor(Math.random() * vocab.length) + 1]
+    window.speechSynthesis.speak(speaker)
+  }
 
   return (
     <div className="app-container">
@@ -27,6 +41,8 @@ function App() {
       >
         Paragraph
       </button>
+      <button onClick={onClick}>Speak</button>
+      <button onClick={onChange}>Random</button>
 
       {mode === inline && <TypingInline />}
       {mode === paragraph && <TypingParagraph />}
