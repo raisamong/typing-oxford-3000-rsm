33d3838cc19f56f2b7e59583f93bbfa960c4e0a8
Thanawat Thangpiboonvech <thanawat.t@dv.co.th>
2021-09-24 12:58:53 +0700

    refactor

--- src/components/TypingInline/index.tsx
+++ src/components/TypingInline/index.tsx
@@ -4,23 +4,48 @@ import { vocab } from 'src/vocab'
 import './style.css'
 
 const whilelistKeyCode = [189, 188, 190]
+const textShowlenght = 30
+
+const matchingText = (leadText: string, textToValidate: string) => {
+  const textToMatched = leadText
+    .substring(0, textToValidate.length)
+    .substring(textToValidate.length - textShowlenght)
+  const textTyped = textToValidate
+    .substring(textToValidate.length - textShowlenght)
+    .split('')
+    .map((letter, index) => {
+      const isMatched = textToMatched.charAt(index) === letter
+      return (
+        <span
+          style={{
+            color: isMatched ? '#7CEC01' : '#000',
+            backgroundColor: isMatched ? '' : 'red',
+          }}
+        >
+          {letter}
+        </span>
+      )
+    })
+
+  return textTyped
+}
 
 const TypingInline = () => {
   const [textToType, setTextToType] = useState<string>('')
-  const [textTyping, setTextTyping] = useState<string>('')
+  const [typedText, setTypedText] = useState<string>('')
 
   useEffect(() => {
     const fn = function (event: { key: any; keyCode: any }) {
       console.log('KeyCode:', event.keyCode)
       if (event.keyCode === 8) {
-        setTextTyping((prev) => prev.substring(0, prev.length - 1))
+        setTypedText((prev) => prev.substring(0, prev.length - 1))
       } else if (
         (event.keyCode >= 48 && event.keyCode <= 90) ||
         whilelistKeyCode.includes(event.keyCode)
       ) {
-        setTextTyping((prev) => prev + event.key)
+        setTypedText((prev) => prev + event.key)
       } else if (event.keyCode === 32) {
-        setTextTyping((prev) => prev + ' ')
+        setTypedText((prev) => prev + ' ')
       }
     }
 
@@ -31,36 +56,21 @@ const TypingInline = () => {
     }
   }, [])
 
-  const textToMatched = textToType.substring(0, textTyping.length).substring(textTyping.length - 30)
-  const textTyped = textTyping
-    .substring(textTyping.length - 30)
-    .split('')
-    .map((letter, index) => {
-      const isMatched = textToMatched.charAt(index) === letter
-      return (
-        <span
-          style={{
-            color: isMatched ? 'green' : '#000',
-            backgroundColor: isMatched ? '' : 'red',
-          }}
-        >
-          {letter}
-        </span>
-      )
-    })
+  const matchedText = matchingText(textToType, typedText)
+
   return (
     <>
       <div className="container">
         <div className="typing-container">
           <div className="typed-text">
             <div className="inner-div">
-              <p className="white-space">{textToMatched}</p>
+              <p className="white-space">{typedText}</p>
             </div>
           </div>
           <div className="typewriter-space"></div>
           <div className="incoming-text">
             <p className="white-space">
-              {textToType.substring(textTyping.length, textTyping.length + 30)}
+              {textToType.substring(typedText.length, typedText.length + textShowlenght)}
             </p>
           </div>
           <div></div>
@@ -68,13 +78,13 @@ const TypingInline = () => {
         <div className="typing-container">
           <div className="typed-text">
             <div className="inner-div">
-              <p className="white-space">{textTyped}</p>
+              <p className="white-space">{matchedText}</p>
             </div>
           </div>
           <div className="typewriter"></div>
           <div className="incoming-text">
             <p className="white-space">
-              {textToType.substring(textTyping.length, textTyping.length + 30)}
+              {textToType.substring(typedText.length, typedText.length + textShowlenght)}
             </p>
           </div>
           <div></div>
